1d537c8b99981a3122ad1af3d2cc15d100633904
numenshane <numenshane@gmail.com>
2016-08-10 10:52:54 +0800

    remove splunk

--- splunk-forwarder-docker/docker_events.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#DOCKER_HOST=tcp://10.77.131.41:2375
-docker -H $DOCKER_HOST events
--- splunk-forwarder-docker/docker_inspect.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker -H $DOCKER_HOST inspect $(docker -H $DOCKER_HOST ps -aq) | jq -c -M -r ".[]"
